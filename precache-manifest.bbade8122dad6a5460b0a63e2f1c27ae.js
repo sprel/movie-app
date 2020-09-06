ca429e55bf1ef74356a6cbca9a10c77c1a539834
prel <black7321@naver.com>
2020-09-06 20:50:07 +0900

    Updates

---
+++ precache-manifest.bbade8122dad6a5460b0a63e2f1c27ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27abe61400aec769c3484acb7325a5b",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "58c4ed4ba47b853e63cd",
+    "url": "/movie-app/static/css/main.673d0850.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c4ed4ba47b853e63cd",
+    "url": "/movie-app/static/js/main.fcf1ae14.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
